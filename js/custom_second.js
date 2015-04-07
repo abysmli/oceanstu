72ff7e3c2478ed6af49ada63391ddb01a698b8a9
abysmli <abysmli@gmail.com>
2015-04-07 03:31:41 +0200

    modified for the require from Qiang

--- js/custom_second.js
+++ js/custom_second.js
@@ -7,70 +7,40 @@
  * License URI:http://wrapbootstrap.com
  * File Description: Place here your custom scripts
  */
-var tpj = jQuery;
-tpj.noConflict();
 
-var image_stu = tpj(".stuttgart img");
-var image_ber = tpj(".berlin img");
-var image_ham = tpj(".hamburg img");
+var image_objs = [];
+image_objs.push($(".stuttgart img"));
+image_objs.push($(".berlin img"));
+image_objs.push($(".hamburg img"));
+image_objs.push($(".frankfurt img"));
+image_objs.push($(".koeln img"));
+image_objs.push($(".muenchen img"));
+image_objs.push($(".heidelberg img"));
+image_objs.push($(".dresden img"));
+image_objs.push($(".hanover img"));
+image_objs.push($(".duesseldorf img"));
 
-tpj(document).ready(function() {
+$(document).ready(function() {
 
-    tpj('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="tooltip"]').tooltip();
 
-    if (tpj.fn.cssOriginal != undefined) tpj.fn.css = tpj.fn.cssOriginal;
-    var api = tpj('.slider-banner').revolution();
-
-    api.bind("revolution.slide.onchange", function(e, data) {
-        if (data.slideIndex == 1) {
-        	changTo_stu(api);
-        }
-        if (data.slideIndex == 2) {
-        	changTo_ber(api);
-        }
-        if (data.slideIndex == 3) {
-        	changTo_ham(api);
-        }
-    });
-
-    var height_deu_map = tpj('.deutschland-map-container').height();
-    var height_slideshow = tpj('.slideshow').height();
-    var margin_top = (height_deu_map - height_slideshow) / 2;
-    if (margin_top > 0) {
-        tpj('.slideshow').css({
-            'margin-top': margin_top + 'px'
+    $.each(image_objs, function(index, obj) {
+        if (!index) obj.attr('src', 'images/point_color.jpg');
+        obj.click(function() {
+            changeTo(index, obj, true);
         });
-    }
+    });
 
-    image_stu.attr('src', 'images/point_color.jpg');
-    image_stu.click(function() {
-        changTo_stu(api);
-    });
-    image_ber.click(function() {
-        changTo_ber(api);
-    });
-    image_ham.click(function() {
-        changTo_ham(api);
-    });
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+        var index = parseInt($(e.target).parent('li').index());
+        changeTo(index, image_objs[index], false);
+    })
 });
 
-function changTo_stu(api) {
-    image_stu.attr('src', 'images/point_color.jpg');
-    image_ber.attr('src', 'images/point_gray.jpg');
-    image_ham.attr('src', 'images/point_gray.jpg');
-    api.revshowslide(1);
-}
-
-function changTo_ber(api) {
-    image_stu.attr('src', 'images/point_gray.jpg');
-    image_ber.attr('src', 'images/point_color.jpg');
-    image_ham.attr('src', 'images/point_gray.jpg');
-    api.revshowslide(2);
-}
-
-function changTo_ham(api) {
-    image_stu.attr('src', 'images/point_gray.jpg');
-    image_ber.attr('src', 'images/point_gray.jpg');
-    image_ham.attr('src', 'images/point_color.jpg');
-    api.revshowslide(3);
+function changeTo(index, obj, flag) {
+    $.each(image_objs, function(_index, _obj) {
+        _obj.attr('src', 'images/point_gray.jpg');
+    });
+    obj.attr('src', 'images/point_color.jpg');
+    if (flag) $('#tab_list li:eq('+index+') a').tab('show');
 }
